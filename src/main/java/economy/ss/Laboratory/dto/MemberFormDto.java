8a53d3388cd6e887e9237bc48d7f8e8c75ee5c73
sixthh-sense <93525885+sixthh-sense@users.noreply.github.com>
2023-07-27 23:55:47 +0900

    validation 기준 변경

--- src/main/java/economy/ss/Laboratory/dto/MemberFormDto.java
+++ src/main/java/economy/ss/Laboratory/dto/MemberFormDto.java
@@ -6,7 +6,6 @@ import org.hibernate.validator.constraints.Length;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotEmpty;
 
 @Getter
 @Setter
@@ -14,11 +13,11 @@ public class MemberFormDto {
     @NotBlank(message = "닉네임은 필수 입력 값입니다.")
     private String username;
 
-    @NotEmpty(message = "이메일은 필수 입력 값입니다.")
+    @NotBlank(message = "이메일은 필수 입력 값입니다.")
     @Email(message = "이메일 형식으로 입력해주세요.")
     private String email;
 
-    @NotEmpty(message = "비밀번호는 필수 입력 값입니다.")
+    @NotBlank(message = "비밀번호는 필수 입력 값입니다.")
     @Length(min = 8, max = 16, message = "비밀번호는 8자 이상, 16자 이하로 입력해주세요.")
     private String password;
 }
