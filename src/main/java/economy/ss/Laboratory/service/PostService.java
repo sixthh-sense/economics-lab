c596eedfc0008ab577747093d5ddf9fd979846f6
sixthh-sense <93525885+sixthh-sense@users.noreply.github.com>
2023-10-23 22:24:29 +0900

    왜 게시물 저장 코드가 없었지(간단하게 추가)

--- src/main/java/economy/ss/Laboratory/service/PostService.java
+++ src/main/java/economy/ss/Laboratory/service/PostService.java
@@ -1,5 +1,7 @@
 package economy.ss.Laboratory.service;
 
+import economy.ss.Laboratory.entity.Post;
+import economy.ss.Laboratory.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -8,4 +10,9 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 @RequiredArgsConstructor
 public class PostService {
+
+    private final PostRepository postRepository;
+    public void savePost(Post post) {
+        postRepository.save(post);
+    }
 }
